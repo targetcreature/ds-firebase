a24e6af12776bdb6a369d793317e6f280900d91a
Craig Hildebrand <targetcreature@gmail.com>
2020-09-04 13:22:39 -0700

    Update firebase!

--- components/Test/index.tsx
+++ components/Test/index.tsx
@@ -1,7 +1,19 @@
+import { useSetData } from "../../context/firebase"
+
 export const Test: React.FC = () => {
 
+    const produce = useSetData()
+
     return (
-        <div>Test</div>
+        <div>
+            <div>Test</div>
+            <button type="button" onClick={() => {
+                produce((draft, uid) => {
+                    draft.players[uid].name = "Tommy"
+                    return draft
+                }, (err) => err && console.log(err))
+            }}>UPDATE</button>
+        </div>
     )
 
 }
--- context/firebase/index.tsx
+++ context/firebase/index.tsx
@@ -1,4 +1,4 @@
-import { createContext, useCallback, useEffect, useState } from "react"
+import { createContext, useCallback, useContext, useEffect, useState } from "react"
 import { Loading } from "../../components/Loading"
 import { newState } from "../init"
 import { State } from "../init/_types"
@@ -8,17 +8,18 @@ type Props = {
     path: string
 }
 
+// const SetDataCTX = createContext<(f: (cb: State) => void) => void>(null)
 const DataCTX = createContext<State>(null)
+const RefCTX = createContext<{ ref: firebase.database.Reference, uid: string }>(null)
 
 export const FirebaseProvider: React.FC<Props> = ({ path, children }) => {
 
     const [connecting, setConnecting] = useState(true)
-
     const [data, setData] = useState(null)
     const [uid, setUID] = useState(null)
     const [isOwner, setOwner] = useState(false)
     const [ref, setRef] = useState<firebase.database.Reference>(null)
-    // const [playerList, setList]
+    const [playerList, setList] = useState([])
 
     console.log({ data, uid, isOwner })
 
@@ -26,45 +27,68 @@ export const FirebaseProvider: React.FC<Props> = ({ path, children }) => {
         ref.set(newState(uid, "Craig"), (err) => err ? console.log(err) : setOwner(true))
     }, [])
 
-    useEffect(() => {
-        firebase.auth().onAuthStateChanged((user) => user && setUID(user.uid))
-    }, [])
-
-    useEffect(() => {
-        if (uid) {
-            let init = false
-            const ref = firebase.database().ref(path)
-            ref.on("value", (snap) => {
-                const data = snap.val()
-                if (!init) {
-                    if (!data) {
-                        createRoom(ref, uid)
-                    }
-                    init = true
-                }
-                setData(snap.val())
-            }, (err) => console.log(err))
-
-            setRef(ref)
-        } else {
-            console.log("authed out")
+    const sortData = useCallback((data: State) => {
+        if (data) {
+            setList(Object.keys(data.players))
+            setData(data)
+            setOwner(data.owner === uid)
         }
-        setConnecting(false)
     }, [uid])
+
+    useEffect(() => {
+        firebase.auth().onAuthStateChanged((user) => {
+            if (user?.uid) {
+                let init = false
+                const ref = firebase.database().ref(path)
+                ref.on("value", (snap) => {
+                    const data = snap.val()
+                    if (!init) {
+                        if (!data) {
+                            createRoom(ref, user.uid)
+                        }
+                        init = true
+                    }
+                    sortData(snap.val())
+                }, (err) => console.log(err))
+                setRef(ref)
+                setUID(user.uid)
+            } else {
+                console.log("authed out")
+            }
+            setConnecting(false)
+        })
+    }, [])
 
     /* on exit */
     const exit = useCallback(() => {
+        if (isOwner) {
+            if (playerList.length > 1) {
+                const newOwner = playerList.map((k) => k !== uid)[0]
+                ref.update({ owner: newOwner })
+            }
+            else {
+                firebase.database().ref(path).remove()
+            }
+            firebase.auth().signOut()
+            console.log("window closed")
+        }
+    }, [isOwner, playerList, ref])
 
-        firebase.database().ref(path).remove()
-        firebase.auth().signOut()
-        console.log("window closed")
-    }, [])
-
-    useEffect(() => window.onbeforeunload = () => exit(), [exit])
+    // useEffect(() => window.onbeforeunload = () => exit(), [exit])
 
     return connecting ? <Loading /> :
-        <DataCTX.Provider value={data}>
-            <button onClick={() => exit()}>DELETE</button>
-            {children}
-        </DataCTX.Provider>
+        <RefCTX.Provider value={{ ref, uid }}>
+            <DataCTX.Provider value={data}>
+                {children}
+            </DataCTX.Provider>
+        </RefCTX.Provider>
+}
+
+export const useSetData = () => {
+    const { ref, uid } = useContext(RefCTX)
+    return (cb: (draft: State, uid: string) => State, onComplete?: (err: Error) => void) => {
+        ref.transaction((state) => {
+            return cb(state, uid)
+        }, (err) => onComplete(err))
+    }
 }
